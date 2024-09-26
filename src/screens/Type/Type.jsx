8436272d70b7e03961b9b452c6fc77aa69f03781
Kejr314 <ejrdud314@gmail.com>
2024-09-26 17:29:05 +0900

    first commit

---
+++ src/screens/Type/Type.jsx
@@ -0,0 +1,201 @@
+import React from "react";
+import "./style.css";
+
+export const Type = () => {
+  return (
+    <div className="type">
+      <div className="div">
+        <footer className="footer">
+          <div className="group">
+            <div className="overlap-group">
+              <div className="text-wrapper">응급NAVI</div>
+              <img className="element" alt="Element" src="/img/1.png" />
+            </div>
+          </div>
+          <div className="group-2">
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <p className="element-KH">
+                  <span className="span">
+                    서울 중구 남대문로 120 대일빌딩 2층, 3층 KH정보교육원 종로지원&nbsp;&nbsp;&nbsp;&nbsp;
+                    |&nbsp;&nbsp;&nbsp;&nbsp; 대표자명 : 민봉식&nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;
+                    대표전화 : 1544-9970
+                    <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp; 2024 응급NAVI.
+                    <br />
+                  </span>
+                </p>
+                <img className="copyright" alt="Copyright" src="/img/copyright-1-1.png" />
+              </div>
+            </div>
+            <div className="text-wrapper-2">
+              이용약관&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;개인정보처리방침
+            </div>
+          </div>
+          <div className="group-3">
+            <img className="instagram" alt="Instagram" src="/img/instagram-1-1.png" />
+            <img className="youtube" alt="Youtube" src="/img/youtube-1-1.png" />
+            <img className="kakaotalk" alt="Kakaotalk" src="/img/kakaotalk-1.png" />
+          </div>
+        </footer>
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="main">
+            <div className="textfield">
+              <div className="navbar">
+                <div className="rectangle-2" />
+                <div className="title">작성자 아이디</div>
+                <div className="title-2">리뷰 내용</div>
+                <div className="title-3">신고자 아이디</div>
+                <div className="title">신고 날짜</div>
+                <div className="title-4">신고 내용</div>
+                <div className="title-5">신고 상태</div>
+                <div className="title">정지 해제 날짜</div>
+              </div>
+              <div className="div-2">
+                <div className="title-6">1</div>
+                <div className="title-7">user01@gmail</div>
+                <div className="title-8">리뷰 내용입니다.</div>
+                <div className="title-9">user02@gmail.com</div>
+                <div className="title-7">2024.09.30</div>
+                <div className="title-10">욕설, 비방</div>
+                <div className="title-11">신고 접수</div>
+                <div className="title-7">-</div>
+                <div className="primary">
+                  <div className="title-12">관리</div>
+                </div>
+              </div>
+              <div className="div-2">
+                <div className="title-6">2</div>
+                <div className="title-7">user01@gmail</div>
+                <div className="title-8">리뷰 내용입니다.</div>
+                <div className="title-9">user02@gmail.com</div>
+                <div className="title-7">2024.09.30</div>
+                <div className="title-10">도배, 스팸</div>
+                <div className="title-11">처리 완료</div>
+                <div className="title-7">2024.10.04</div>
+                <div className="primary">
+                  <div className="title-12">관리</div>
+                </div>
+              </div>
+              <div className="div-2">
+                <div className="title-6">3</div>
+                <div className="title-7">user01@gmail</div>
+                <div className="title-8">리뷰 내용입니다.</div>
+                <div className="title-9">user02@gmail.com</div>
+                <div className="title-7">2024.09.30</div>
+                <div className="title-10">욕설, 비방</div>
+                <div className="title-11">처리 완료</div>
+                <div className="title-7">2024.10.04</div>
+                <div className="primary">
+                  <div className="title-12">관리</div>
+                </div>
+              </div>
+              <div className="div-2">
+                <div className="title-6">4</div>
+                <div className="title-7">user01@gmail</div>
+                <div className="title-8">리뷰 내용입니다.</div>
+                <div className="title-9">user02@gmail.com</div>
+                <div className="title-7">2024.09.30</div>
+                <div className="title-10">욕설, 비방</div>
+                <div className="title-11">처리 완료</div>
+                <div className="title-7">2024.10.04</div>
+                <div className="primary">
+                  <div className="title-12">관리</div>
+                </div>
+              </div>
+              <div className="div-2">
+                <div className="title-6">5</div>
+                <div className="title-7">user01@gmail</div>
+                <div className="title-8">리뷰 내용입니다.</div>
+                <div className="title-9">user02@gmail.com</div>
+                <div className="title-7">2024.09.30</div>
+                <div className="title-10">욕설, 비방</div>
+                <div className="title-11">신고 접수</div>
+                <div className="title-7">-</div>
+                <div className="primary">
+                  <div className="title-12">관리</div>
+                </div>
+              </div>
+              <div className="div-2">
+                <div className="title-6">6</div>
+                <div className="title-7">user01@gmail</div>
+                <div className="title-8">리뷰 내용입니다.</div>
+                <div className="title-9">user02@gmail.com</div>
+                <div className="title-7">2024.09.30</div>
+                <div className="title-10">욕설, 비방</div>
+                <div className="title-11">신고 접수</div>
+                <div className="title-7">-</div>
+                <div className="primary">
+                  <div className="title-12">관리</div>
+                </div>
+              </div>
+            </div>
+            <div className="frame">
+              <div className="div-wrapper">
+                <div className="overlap-group-3">
+                  <div className="text-wrapper-3">1</div>
+                </div>
+              </div>
+              <div className="overlap-wrapper">
+                <div className="overlap-2">
+                  <div className="text-wrapper-4">2</div>
+                </div>
+              </div>
+              <div className="group-4">
+                <div className="overlap-2">
+                  <div className="text-wrapper-4">3</div>
+                </div>
+              </div>
+              <div className="group-5">
+                <div className="overlap-2">
+                  <div className="text-wrapper-4">4</div>
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-5">회원 신고 리스트</div>
+          </div>
+          <div className="header-logined">
+            <div className="overlap-3">
+              <div className="group-6">
+                <div className="overlap-group-4">
+                  <div className="rectangle-3" />
+                  <p className="p">
+                    <span className="text-wrapper-6">|</span>
+                    <span className="text-wrapper-7">&nbsp;&nbsp; </span>
+                    <span className="text-wrapper-8">마이페이지</span>
+                    <span className="text-wrapper-7">&nbsp;&nbsp; </span>
+                    <span className="text-wrapper-6">|</span>
+                    <span className="text-wrapper-7">&nbsp;&nbsp; </span>
+                    <span className="text-wrapper-8">로그아웃</span>
+                    <span className="text-wrapper-7">&nbsp;&nbsp; </span>
+                    <span className="text-wrapper-6">|</span>
+                  </p>
+                </div>
+              </div>
+              <div className="group-7">
+                <div className="overlap-4">
+                  <div className="rectangle-4" />
+                  <div className="AED">
+                    주변응급실&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;주변병원&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;주변약국&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    자동제세동기(AED)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;이용안내&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;공지사항
+                  </div>
+                  <div className="group-8">
+                    <div className="group-wrapper">
+                      <div className="group-9">
+                        <div className="overlap-group-5">
+                          <div className="text-wrapper-9">응급NAVI</div>
+                          <img className="file" alt="File" src="/img/file-1.png" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
